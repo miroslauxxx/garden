ea8145bc07e329dcb7be2387f1fe06a47990fe33
roy <robert@666.support>
2026-07-19 22:34:02 +0200

    did something

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,13 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: []
+  afterBody: [],
+  footer: Component.Footer({
+    links: {
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
+    },
+  }),
 }
 
 // components for pages that display a single page (e.g. a single note)
